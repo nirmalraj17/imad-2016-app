3939ade047d98b5ddad29a9337da1d1196349e62
nirmalraj17 <nirmalraj17@gmail.com>
2016-09-23 00:05:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
+
+//move image
+var img = document.getElementById('madi');
+img.onClick = function (){
+        img.style.marginLeft = '100px';
+};
