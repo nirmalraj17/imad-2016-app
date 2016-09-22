ae5cec5f85991b3accdf2e5b4d7a4cc1f33f03ac
nirmalraj17 <nirmalraj17@gmail.com>
2016-09-23 00:13:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ element.innerHTML = "New Value";
 
 //move image
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveRight() {
+    marginleft = marginleft + 10;
+    img.style.marginLeft = marginleft + 'px';
+}
 img.onclick = function (){
-        img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
