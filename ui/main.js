a055f1d6f0225d8f67d15ce5c6b2793669eaf74e
nirmalraj17 <nir1988NR>
2016-09-25 20:39:47 +0530

    Local changes

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,16 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value";
-
-//move image
-var img = document.getElementById('madi');
-var marginleft = 0;
-function moveRight() {
-    marginleft = marginleft + 1;
-    img.style.marginLeft = marginleft + 'px';
-}
-img.onclick = function (){
-    var interval = setInterval(moveRight, 50);
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function(){
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function(){
+		if (request.readyState === XMLHttpRequest.DONE){
+			if (request.status === 200){
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter;	
+			}
+		} 
+	};
+	request.open('GET', 'http://localhost/counter',true);
+	request.send(null);
 };
