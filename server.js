ba20d1aec6032d86df443d4bca8a22611ee6b057
nirmalraj17 <nirmalraj17@gmail.com>
2016-11-18 00:02:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+   user: "nirmalraj17",
+   database: "nirmalraj17",
+   host: "db.imad.hasura-app.io",
+   port: "5432",
+   password: process.env.DB_PASSWORD
+   
+};
 var app = express();
 app.use(morgan('combined'));
+
+
 
 var articles = {
     'article-one': {
@@ -82,6 +93,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db', function (req,res) {
+    pool.query("select * from test", function (err, result){
+       if (err){
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+    });
+});
 
 var counter = 0;
 app.get('/counter', function(req,res){
