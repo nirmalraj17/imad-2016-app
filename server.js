dbf6a46a3aa63e7280234ac015da5577e48c62bf
nirmalraj17 <nirmalraj17@gmail.com>
2016-11-18 00:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,6 @@ app.get('/counter', function(req,res){
 
 app.get('/articles/:articleName', function(req,res){
  
-  var articleData = 
   pool.query("select * from article where title = " + req.params.articleName , function (err, result){
        if (err){
            res.status(500).send(err.toString());
@@ -120,7 +119,7 @@ app.get('/articles/:articleName', function(req,res){
            if (result.rows.length === 0) {
                res.status(404).send('Article not found');
            } else {
-                articleData = result.rows[0];
+                var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
            }
        }
