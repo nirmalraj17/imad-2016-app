431ea3f180922f0d45a3de497717ba4600ece043
nirmalraj17 <nirmalraj17@gmail.com>
2016-11-19 01:11:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,42 +1,27 @@
-var counter = 0;
-var button = document.getElementById('counter');
-button.onclick = function(){
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function(){
-		if (request.readyState === XMLHttpRequest.DONE){
-			if (request.status === 200){
-				var counter = request.responseText;
-				var span = document.getElementById('count');
-				span.innerHTML = counter;	
-			}
-		} 
-	};
-	request.open('GET', 'http://localhost/counter',true);
-	request.send(null);
-};
-
-
-
-var submit =document.getElementById('submit');
+var submit =document.getElementById('submit_btn');
 submit.onclick = function(){
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function(){
 		if (request.readyState === XMLHttpRequest.DONE){
 			if (request.status === 200){
-				var names = request.responseText;
-				names = JSON.parse(names);
-				var list = '';
-				for (var i = 0; i< names.length; i++){
-					list += '<li>' + names[i] + '</li>';
-				}
-				var ul = document.getElementById('namelist');
-				ul.innerHTML = list;
+				console.log('user loged in');
+				alert('Logged in successfully');
+			} else if (request.status === 403) {
+			    alert('Incorrect user naeme or credentials');
+			} else if (request.status === 500) {
+			    alert('Something went wrong!!');
 			}
 		} 	
 	};
-	var  nameInput = document.getElementById('name');
-	var name = nameInput.value;
-	request.open('GET', 'http://localhost/submit-name?name=' + name,true);
-	request.send(null);
+	var  username = document.getElementById('username').value;
+	var  password = document.getElementById('password').value;
+	
+	console.log(username);
+	console.log(password);
+	
+	var name = username.value;
+	request.open('POST', 'http://nirmalraj17.imad.hasura-app.io/login' ,true);
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.send(JSON.stringify({username: username, password: password}));
 	
 };
