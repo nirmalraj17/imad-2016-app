8fe00200fb7bc7c23a69b0e5c0b69c7c895f303c
nirmalraj17 <nirmalraj17@gmail.com>
2016-11-19 01:21:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,6 +157,11 @@ app.get('/check-login', function (req,res){
    }
 });
 
+app.get('/logout', function (req, res){
+   delete req.session.auth;
+   req.send('Logged out');
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function (req,res) {
     pool.query("select * from test", function (err, result){
