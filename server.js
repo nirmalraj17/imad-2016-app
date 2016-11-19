5d909901658fd9858ad34ebb6f0853645ae7e8bc
nirmalraj17 <nirmalraj17@gmail.com>
2016-11-19 17:06:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,24 +23,6 @@ app.use(session({
     ccookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-function myFunction() {
-    alert(document.getElementById("userid").value + " " +  document.getElementById("psw").value);
-}
-function visible(id) {
-    document.getElementById(id).style.visibility = "visible" ;
-    document.getElementById("signin").style.display ="block";
-    if (id == "signin"){
-        document.getElementById("signup").style.visibility = "hidden" ;
-    } else {
-        document.getElementById("signin").style.display = "none" ;
-    }
-}
-
-function invisible(){
-    document.getElementById("signin").style.visibility = "hidden" ;
-    document.getElementById("signup").style.visibility = "hidden" ;
-}
-
 var articles = {
     'article-one': {
         title: 'Article One | Nirmal Raj',
