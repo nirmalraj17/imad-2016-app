bc91f90ed77dac1d4d7f247c20f675e1f6b9d9f4
nirmalraj17 <nirmalraj17@gmail.com>
2016-11-19 23:09:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,12 +24,6 @@ app.use(session({
 }));
 
 
-function myFunction() {
-    alert(document.getElementById("userid").value + " " +  document.getElementById("psw").value);
-}
-
-
-
 var articles = {
     'article-one': {
         title: 'Article One | Nirmal Raj',
@@ -209,6 +203,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/scripts.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'scripts.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
